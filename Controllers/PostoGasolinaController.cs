785b75f60302995d5de6014598e55e7d3fc6ddbd
Zubizeru <jose.ehmendes@senacsp.edu.br>
2025-05-07 21:29:03 -0300

    feat: 100% atualizado

--- Controllers/PostoGasolinaController.cs
+++ Controllers/PostoGasolinaController.cs
@@ -11,5 +11,38 @@ namespace PostoGasolinaAPI.Controllers
         {
             return StatusCode(200, BancoDeDados.Combustiveis);
         }
-    }
+
+        [HttpPost("Comprar Combustivel")]
+        public IActionResult ComprarCombustivel (int codigoDeCombustivel, double litros)
+        {
+            Combustivel? combustivelEscolhido = null;
+
+            foreach (Combustivel combustivel in BancoDeDados.Combustiveis)
+            {
+                if(combustivel.CodigoDoProduto == codigoDeCombustivel)
+                {
+                    combustivelEscolhido = combustivel;
+                    break;
+                }
+            }
+
+            if (combustivelEscolhido == null)
+                return StatusCode(400, "Nenhum código foi encontrado");
+
+            Compra compra = new Compra();
+            compra.Combustivel = combustivelEscolhido;
+            compra.DataCompra = DateTime.Now;
+            compra.ValorTotal = combustivelEscolhido.PrecoLitro * litros;
+
+            BancoDeDados.Compras.Add(compra);
+
+            return StatusCode(200, compra);
+        }
+
+        [HttpGet("Extrato")]
+        public IActionResult Extrato()
+        {
+            return StatusCode(200, BancoDeDados.Compras);
+        }
+    } 
 }
